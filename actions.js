8042c44e9a3c24a9717818b2ebb2d5fa89a6e972
JeLuF <jf@mormo.org>
2023-10-22 01:26:24 +0200

    make function async

--- actions.js
+++ actions.js
@@ -1,6 +1,6 @@
 
 module.exports = function (self) {
-        function tv_do_command(cmd) {
+        async function tv_do_command(cmd) {
             const data = await got({
                 url: 'http://198.18.10.10/cmdparse',
                 method: 'POST',
@@ -26,7 +26,7 @@ module.exports = function (self) {
 				},
 			],
 			callback: async (event) => {
-                                tv_do_command("Left")
+                                await tv_do_command("Left")
 			},
 		},
 		right_action: {
@@ -42,7 +42,7 @@ module.exports = function (self) {
 				},
 			],
 			callback: async (event) => {
-                                tv_do_command("Right")
+                                await tv_do_command("Right")
 			},
 		},
 		down_action: {
@@ -58,7 +58,7 @@ module.exports = function (self) {
 				},
 			],
 			callback: async (event) => {
-                                tv_do_command("Down")
+                                await tv_do_command("Down")
 			},
 		},
 		up_action: {
@@ -74,7 +74,7 @@ module.exports = function (self) {
 				},
 			],
 			callback: async (event) => {
-                                tv_do_command("Up")
+                                await tv_do_command("Up")
 			},
 		},
 	})
